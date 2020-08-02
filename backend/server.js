ea86c0e00b52c46d1c0d8425b745a247238a6078
udasitharani <udasitharani@gmail.com>
2020-08-03 00:15:05 +0530

    setup firebase-admin

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+const FirebaseAdmin = require("firebase-admin");
+const serviceAccount = require("./ServiceAccountKey.json");
+FirebaseAdmin.initializeApp({
+  credential: FirebaseAdmin.credential.cert(serviceAccount),
+});
+
 app.get("/", (req, res) => {
   res.send("hello world!");
 });
