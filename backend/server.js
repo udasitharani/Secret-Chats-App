3f79da721fc35aa79d0fc92b5c6b95809785429b
udasitharani <udasitharani@gmail.com>
2020-08-03 00:08:43 +0530

    initial express boilerplate

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("hello world!");
+});
+
+app.listen(PORT);
