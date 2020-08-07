296f600aed10c3f4b0d9ab1cfe3aeb483d499c8b
udasitharani <udasitharani@gmail.com>
2020-08-07 17:09:01 +0530

    created and setup RoomDataContext

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,39 +8,43 @@ import CreateRoom from "./Components/CreateRoom/CreateRoom";
 import JoinRoom from "./Components/JoinRoom/JoinRoom";
 import ChatRoom from "./Components/ChatRoom/ChatRoom";
 import TitleContext from "./contexts/TitleContext";
+import RoomDataContext from "./contexts/RoomDataContext";
 
 const App = () => {
   const [headerTitle, setHeaderTitle] = useState("Secrets");
-
+  const [roomData, setRoomData] = useState({});
   const titleContext = { headerTitle, setHeaderTitle };
+  const roomDataContext = { roomData, setRoomData };
 
   return (
     <TitleContext.Provider value={titleContext}>
-      <div style={{ height: "100vh" }} className="App">
-        <Grid
-          container
-          style={{ height: "100%" }}
-          direction="column"
-          justify="flex-start"
-          align-items="center"
-        >
-          <div className="app-header">
-            <AppHeader />
-          </div>
-          <div className="app-body" style={{ flex: "1 1 auto" }}>
-            <Switch>
-              <Route path="/" component={Home} exact />
-              <Route path="/create-room">
-                <CreateRoom />
-              </Route>
-              <Route path="/join-room">
-                <JoinRoom />
-              </Route>
-              <Route path="/chat-room" component={ChatRoom} />
-            </Switch>
-          </div>
-        </Grid>
-      </div>
+      <RoomDataContext.Provider value={roomDataContext}>
+        <div style={{ height: "100vh" }} className="App">
+          <Grid
+            container
+            style={{ height: "100%" }}
+            direction="column"
+            justify="flex-start"
+            align-items="center"
+          >
+            <div className="app-header">
+              <AppHeader />
+            </div>
+            <div className="app-body" style={{ flex: "1 1 auto" }}>
+              <Switch>
+                <Route path="/" component={Home} exact />
+                <Route path="/create-room">
+                  <CreateRoom />
+                </Route>
+                <Route path="/join-room">
+                  <JoinRoom />
+                </Route>
+                <Route path="/chat-room" component={ChatRoom} />
+              </Switch>
+            </div>
+          </Grid>
+        </div>
+      </RoomDataContext.Provider>
     </TitleContext.Provider>
   );
 };
---
+++ frontend/src/contexts/RoomDataContext.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const RoomDataContext = React.createContext();
+
+export default RoomDataContext;
